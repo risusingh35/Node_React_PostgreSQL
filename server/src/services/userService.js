d866c0c3656ce42ca71efb0a7342158c37f1ee31
Risu Singh <risusingh35@gmail.com>
2024-07-31 12:29:16 +0530

    user crud done in posrgresql

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,22 +1,24 @@
 const { User } = require('../../models/index');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {auth} = require('../../config/index');
-
+const { auth } = require('../../config/index');
+const { pgClient } = require('../../db/index')
+const { v4: uuidv4 } = require('uuid');
 const saltRounds = auth.saltRounds;
 const jwtSecret = auth.jwtSecret;
 const jwtExpiration = auth.jwtExpiration;
 const refreshJwtSecret = auth.refreshJwtSecret;
-const refreshJwtExpiration=auth.refreshJwtExpiration
+const refreshJwtExpiration = auth.refreshJwtExpiration
 const createUser = async (userData) => {
   try {
-    const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
-    const newUser = new User({
-      ...userData,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    return newUser;
+    // const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
+    // const newUser = new User({
+    //   ...userData,
+    //   password: hashedPassword,
+    // });
+    // await newUser.save();
+    // return newUser;
+    return getCreatePgUsers(userData);
   } catch (error) {
     throw new Error('Error creating user: ' + error.message);
   }
@@ -25,7 +27,8 @@ const createUser = async (userData) => {
 const getAllUsers = async () => {
   try {
     const users = await User.find({});
-    return users;
+    // return users;//mongodb
+    return getALlPgUsers();
   } catch (error) {
     throw new Error('Error fetching users: ' + error.message);
   }
@@ -33,7 +36,8 @@ const getAllUsers = async () => {
 
 const getUserById = async (id) => {
   try {
-    const user = await User.findById(id);
+    // const user = await User.findById(id);
+    const user = getByIdPgUsers(id);
     if (!user) throw new Error('User not found');
     return user;
   } catch (error) {
@@ -43,10 +47,11 @@ const getUserById = async (id) => {
 
 const updateUser = async (id, updateData) => {
   try {
-    const user = await User.updateOne({_id:id}, updateData);
-    
-    if (!user) throw new Error('User not found');
-    return user;
+    // const user = await User.updateOne({ _id: id }, updateData);
+
+    // if (!user) throw new Error('User not found');
+    // return user;
+    return getUpdatePgUsers(id, updateData)
   } catch (error) {
     console.error('Error updating user:', error.message); // Log the error
     throw new Error('Error updating user: ' + error.message);
@@ -56,7 +61,8 @@ const updateUser = async (id, updateData) => {
 
 const deleteUser = async (id) => {
   try {
-    const user = await User.deleteOne({_id:id});
+    // const user = await User.deleteOne({ _id: id });
+    const user =deleteByIdPgUsers(id);
     if (!user) throw new Error('User not found');
     return user;
   } catch (error) {
@@ -70,8 +76,8 @@ const authenticateUser = async (email, password) => {
     throw new Error('Invalid credentials');
   }
 
-  const token = jwt.sign({ id: user._id },jwtSecret, { expiresIn: jwtExpiration });
-  const refreshToken = jwt.sign({ id: user._id }, refreshJwtSecret, { expiresIn: refreshJwtExpiration});
+  const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: jwtExpiration });
+  const refreshToken = jwt.sign({ id: user._id }, refreshJwtSecret, { expiresIn: refreshJwtExpiration });
 
   const userResponse = {
     _id: user._id,
@@ -90,6 +96,58 @@ const refreshToken = (refreshToken) => {
     throw new Error('Invalid refresh token');
   }
 };
+
+const getALlPgUsers = async () => {
+  try {
+    const result = await pgClient.query('SELECT * FROM "user"');
+    console.log({ getALlPgUsers: result.rows });
+    return result.rows
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+const getByIdPgUsers = async (id) => {
+  try {
+    const result = await pgClient.query('SELECT * FROM "user" WHERE "id" = $1', [id]);
+    console.log({ getByIdPgUsers: result.rows[0] });
+    if (result.rows.length > 0) {
+      return result.rows[0]
+    }
+    return null
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+const deleteByIdPgUsers = async (id) => {
+  try {
+    const result =await pgClient.query('DELETE FROM "user" WHERE "id" = $1', [id]);
+    console.log({ deleteByIdPgUsers: result });
+   return result
+    return null
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+const getUpdatePgUsers = async (id, updateData) => {
+  try {
+    const result = await pgClient.query('UPDATE "user" SET "name" = $1 WHERE "id" = $2 RETURNING *', [updateData.name, id]);
+    console.log({ getUpdatePgUsers: result.rows[0] });
+    return result.rows[0]
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+const getCreatePgUsers = async (userData) => {
+  const uuid = uuidv4()
+  try {
+    const result = await pgClient.query('INSERT INTO "user" ("id", "name") VALUES ($1, $2) RETURNING *', [uuid, userData.name]);
+
+    console.log({ getCreatePgUsers: result.rows[0] });
+    return result.rows[0]
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
 
 module.exports = {
   createUser,
